96e642a2f2668997c328bf7d1faa5f796d42d192
Kiveo <mavericktesini@gmail.com>
2018-08-07 12:14:02 -0700

    add params method and create logic in instructor controller

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -5,10 +5,22 @@ class InstructorsController < ApplicationController
   end 
 
   def new
-    @instructor = Instructor.new 
+    if logged_in?
+      redirecto_to root_url
+    else 
+      @instructor = Instructor.new 
+    end 
   end 
 
   def create
+    @instructor.new(instructor_params)
+    if @instructor.save && @instructor.authenticate(params[:password])
+      flash[:notice] = 'Instructor successfully registered'
+      redirecto_to root_url
+    else 
+      flash[:notice] = @instructor.errors.full_messages
+      redirecto_to new_instructor_path
+    end 
   end 
 
   def show
@@ -21,6 +33,12 @@ class InstructorsController < ApplicationController
   end 
 
   def delete
+  end 
+
+  private 
+
+  def instructor_params
+    params.require(:instructor).permit(:name, :cfi, :password)
   end 
 
 end 
