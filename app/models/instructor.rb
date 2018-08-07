206c7f023ca809c41191db405d72b0f70ee720c1
Kiveo <mavericktesini@gmail.com>
2018-08-07 12:33:13 -0700

    add validations to instructor model

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -4,6 +4,15 @@ class Instructor < ActiveRecord::Base
 
   has_secure_password
 
+  validates :name, presence: true
+  validates :cfi, presence: true
+  validates :cfi,   numericality: {only_integer: true}
+  validates :cfi, length: {minimum: 1}
+  validates :cfi, uniqueness: true 
+  validates :password, presence: true
+  validates :password, length {minimum: 1}
+
+
 
   def students_scheduled
     self.lessons.size
