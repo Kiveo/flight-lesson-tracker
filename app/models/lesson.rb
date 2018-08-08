ce6ac5e94e99860eb27edea78a8cdca3497ea1ad
Kiveo <mavericktesini@gmail.com>
2018-08-07 20:25:28 -0700

    add validations for lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,11 @@ class Lesson < ActiveRecord::Base
   belongs_to :student
   belongs_to :instructor
 
+  validates :instructor_id, presence: true
+  validates :instructor_id, numericality: {greater_than: 1}
+  validates :student_id, presence: true 
+  validates :student_id, numericality: {greater_than: 1}
+  validates :description, presence: true 
+  validates :lesson_datetime, presence: true 
+
 end
