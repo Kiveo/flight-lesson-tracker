8744697fba72ad1d7e0d5a3a90a22b71c1d1000a
Kiveo <mavericktesini@gmail.com>
2018-08-07 18:47:56 -0700

    add lesson time to show page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -11,18 +11,37 @@ class LessonsController < ApplicationController
   end 
 
   def show
+    @lesson = Lesson.find(params[:id])
   end 
 
   def edit
-    @lesson = Lesson.find(params[:id])
-    @students = Student.all 
-    @instructors = Instructor.all 
+    if logged_in?
+      @lesson = Lesson.find(params[:id])
+      @students = Student.all 
+      @instructors = Instructor.all
+    else 
+      redirect_to '/login'
+    end  
   end 
 
-  def update 
+  def update
+    @lesson = Lesson.new(lesson_params)
+    if @lesson.save
+      flash[:notice] = "Successfully updated lesson."
+      redirect_to lesson_path(@lesson)
+    else 
+      flash[:alert] = "Invalid Data, please try again."
+      render lesson_edit_path(@lesson)
+    end  
   end 
 
   def delete
   end 
   
+  private 
+
+  def lesson_params
+    params.require(:lesson).permit(:description, :instructor_id, :student_id)
+  end 
+
 end
