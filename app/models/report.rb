6c3303c7d183b258105c4c3f89181cb54210c69f
Kiveo <mavericktesini@gmail.com>
2018-08-08 20:23:52 -0700

    add validations to report model

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,8 @@
 class Report < ActiveRecord::Base
+  belongs_to :instructor
+
+  validates :title, presence: true
+  validates :flight_hours, presence: true 
+  validates :flight_hours, numericality: true
+  
 end
