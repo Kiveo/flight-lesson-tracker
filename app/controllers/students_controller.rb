d04f05c074f753aa1b99db4f124aa22eb128bd2a
Kiveo <mavericktesini@gmail.com>
2018-08-08 21:19:08 -0700

    associate, validate, and route new student functionality

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,9 +4,22 @@ class StudentsController < ApplicationController
   end 
 
   def new
+    @student = Student.new 
   end 
 
   def create
+    if logged_in?
+      @student = Student.new(student_params)
+      if @student.save 
+        flash[:notice] = "Successfully registered student"
+        redirect_to student_path(@student)
+      else 
+        flash[:alert] = @student.errors.full_messages.join(", ")
+        render new_student_path
+      end
+    else 
+      redirect_to '/login'
+    end  
   end 
 
   def show
--- app/models/student.rb
+++ app/models/student.rb
@@ -3,3 +3,10 @@ class Student < ActiveRecord::Base
   has_many :instructors, through: :lessons
 
 end
+class Student < ActiveRecord::Base 
+  has_many :lessons 
+  has_many :instructors, through: :lessons
+
+  validates :name, presence: true
+  
+end
