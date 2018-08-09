bbf74ad0afee97e11a5c13e3d8119362f915982e
Kiveo <mavericktesini@gmail.com>
2018-08-08 21:49:02 -0700

    add validations for student_id to student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -8,5 +8,8 @@ class Student < ActiveRecord::Base
   has_many :instructors, through: :lessons
 
   validates :name, presence: true
+  validates :student_id, numericality: {greater_than: 0}
+  validates :student_id, numericality: {only_integer: true}
+  validates :student_id, uniqueness: true
   
 end
