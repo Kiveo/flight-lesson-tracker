04616af609db22b60e1c33441d16753b096db598
Kiveo <mavericktesini@gmail.com>
2018-08-08 21:46:46 -0700

    add column to students table via migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180808225136) do
+ActiveRecord::Schema.define(version: 20180809044447) do
 
   create_table "instructors", force: :cascade do |t|
     t.string  "name"
@@ -35,7 +35,8 @@ ActiveRecord::Schema.define(version: 20180808225136) do
   end
 
   create_table "students", force: :cascade do |t|
-    t.string "name"
+    t.string  "name"
+    t.integer "student_id"
   end
 
 end
