eacdd0d9e380295dcb3000e6d72cb1bd1162b932
Kiveo <mavericktesini@gmail.com>
2018-08-09 14:35:38 -0700

    add link for github login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,23 @@ class SessionsController < ApplicationController
   def new
   end 
   
-  def create #instructors will likely have a unique FAA CFI number 
-    instructor = Instructor.find_by(cfi: params[:session][:cfi])
-    if instructor && instructor.authenticate(params[:session][:password])
-      log_in instructor
-      flash[:notice] = "Successfully Logged In."
+  def create  
+    #omniauth login
+    if auth_hash = request.env["omniauth.auth"] 
+      @instructor = Instructor.find_or_create_from_auth_hash(auth_hash)
+      log_in @instructor
+      flash[:notice] = "Welcome Back!"
       redirect_to instructor
-    else
-      flash.now[:alert] = 'Invalid email/password combination'
-      render 'new'
+    else #normal login  
+      instructor = Instructor.find_by(cfi: params[:session][:cfi])
+      if instructor && instructor.authenticate(params[:session][:password])
+        log_in instructor
+        flash[:notice] = "Successfully Logged In."
+        redirect_to instructor
+      else
+        flash.now[:alert] = 'Invalid email/password combination'
+        render 'new'
+      end 
     end 
   end 
 
@@ -29,4 +37,11 @@ class SessionsController < ApplicationController
   def log_out
     session.delete(:user_id)
   end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+
 end 
