faa002ec8f29a472cb01cc2abc11f40945054d83
Kiveo <mavericktesini@gmail.com>
2018-08-09 18:03:05 -0700

    fix bug in student seed, add report seed

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,3 @@
-class Student < ActiveRecord::Base
-  has_many :lessons
-  has_many :instructors, through: :lessons
-
-end
 class Student < ActiveRecord::Base 
   has_many :lessons 
   has_many :instructors, through: :lessons
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # create students
-students_seed = Student.create([{ name: 'Alpha' }, { name: 'Bravo' }, {name: 'Charley'}])
+students_seed = Student.create([{ name: 'Alpha', student_id: 1 }, { name: 'Bravo', student_id: 2 }, {name: 'Charley', student_id: 3}])
 # create some instructors
 instructors_seed = Instructor.create([
-  {name: 'Instructor 1', cfi: 00123, password: 'password1'}, 
-  {name: 'Instructor 2', cfi: 00234, password: 'password2'},
-  {name: 'Instructor 3', cfi: 00345, password: 'password3'}
+  {name: 'Instructor 1', cfi: 123, password: 'password1'}, 
+  {name: 'Instructor 2', cfi: 234, password: 'password2'},
+  {name: 'Instructor 3', cfi: 345, password: 'password3'}
 ])
 
 lessons_seed = Lesson.create([
@@ -23,3 +23,7 @@ lessons_seed = Lesson.create([
   {instructor_id: 3, student_id: 3, description: "Lesson 3-B", lesson_datetime: DateTime.new(2018,5,11,4)},
   {instructor_id: 3, student_id: 3, description: "Lesson 3-C", lesson_datetime: DateTime.new(2019,4,10,3)}
 ])
+
+report_seed1 = Report.create(title: 'Report 1', flight_hours: 11, ground_hours: 5, instructor_id: 1)
+report_seed2 = Report.create(title: 'Report 2', flight_hours: 22, ground_hours: 2, instructor_id: 1)
+report_seed3 = Report.create(title: 'Report 3', flight_hours: 3, ground_hours: 33, instructor_id: 1)
