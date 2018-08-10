82e5d5176e544f10befe8b0726bb08247920e8fa
Kiveo <mavericktesini@gmail.com>
2018-08-09 18:20:51 -0700

    remove students helper

--- app/helpers/instructors_helper.rb
+++ app/helpers/instructors_helper.rb
@@ -1,8 +1,8 @@
 module InstructorsHelper
   
-  def flash_message(flash)
-    "<div class='field_with_errors'>#{flash}</div>"
-  end
+  # def flash_message(flash)
+  #   "<div class='field_with_errors'>#{flash}</div>"
+  # end
 
   def important_paragraph
     "Important: FAA CFI number will be used for login."  
