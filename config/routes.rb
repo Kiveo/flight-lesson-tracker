6ea352a563dcf613f3dc8887001ecaf99e15dd72
Kiveo <mavericktesini@gmail.com>
2018-08-09 18:12:45 -0700

    refactor student index and show views, add partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root 'welcome#index'
 
   resources :students
@@ -16,20 +15,5 @@ Rails.application.routes.draw do
   delete '/logout', to: "sessions#delete"
 
   get '/auth/github/callback', to: 'sessions#create'
-  # get '/auth/:provider/callback', :to => 'sessions#create'
-  # get 'http://localhost:3000/auth/github/callback', to: 'sessions#create'
-
-  # resources :sessions, only: [:new, :create, :delete]
- 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
 
 end
