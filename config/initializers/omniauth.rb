664bdef51365ebd3e225c39286e0bf97720bfccb
Mav <30327928+Kiveo@users.noreply.github.com>
2018-08-14 21:19:44 -0400

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer unless Rails.env.production?
-  provider :github, 'b94b4c85eeb2abf042ab', '151f531bbcad077a93a2f9039110c2cfa6ecb13a'
+  provider :github, 'useyourkey', 'useyourpasskey'
 end
