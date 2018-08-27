436bbbc6ffc8c822b96caaa0287169e361789207
Maverick Tesini <mavericktesini@gmail.com>
2018-08-27 18:30:14 -0400

    add student relationship to lesson serializer

--- app/serializers/lesson_serializer.rb
+++ app/serializers/lesson_serializer.rb
@@ -1,4 +1,5 @@
 class LessonSerializer < ActiveModel::Serializer
   attributes :id, :description, :lesson_datetime, :student_id, :instructor_id
-  belongs_to :instructor 
+  belongs_to :instructor
+  belongs_to :student 
 end
