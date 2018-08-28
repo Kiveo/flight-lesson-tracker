6395776a702c2570b3ceca05b206662688bf427e
Maverick Tesini <mavericktesini@gmail.com>
2018-08-27 21:59:16 -0400

    flesh out prototype for lesson on show view/next functionality

--- app/assets/javascripts/lesson.js
+++ app/assets/javascripts/lesson.js
@@ -7,15 +7,28 @@ $(function() {
   });
 })
 
+// GET the next lesson from the controller
 function nextLesson(url) {
   console.log(`clicked the next link`);
-  $.get(url, function(data) {
-    jsLesson(data);
-  })
+  $.get(url, function() {
+    console.log("data requested");
+  }).done(function(data) {
+    var jslesson = new Lesson(data)
+    // var lessonDisplay = jslesson.renderLesson()
+  });
 }
 
-// create an object from the data
-function jsLesson(data) {
-  console.log("You called jsLesson!");
-  console.log(data);
+// create an object from the data: Prototype for the lesson
+function Lesson(attributes) {
+  console.log("You called Lesson Prototype");
+  console.log(attributes);
+  this.student = attributes.student.name;
+  this.instructor = attributes.instructor.name;
+  this.description = attributes.description;
+  this.lesson_datetime = attributes.lesson_datetime;
+   
+  console.log(this.student);
+  console.log(this.instructor);
+  console.log(this.description);
+  console.log(this.lesson_datetime);
 }
