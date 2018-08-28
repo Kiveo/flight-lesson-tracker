b16230611f13f464d3bcc770b00ade28d0a982ab
Maverick Tesini <mavericktesini@gmail.com>
2018-08-27 20:07:59 -0400

    replaced button with hyperlink on lesson show

---
+++ app/assets/javascripts/lesson.js
@@ -0,0 +1,13 @@
+// setup listener, after page load
+$(function() {
+  $('#nextLesson').on('click', function(e) {
+    e.preventDefault();
+    nextLesson();
+  });
+})
+
+function nextLesson() {
+  // get show for json from controller
+  console.log(`clicked the next link`);
+  // $.get(`/lessons/<%= @lesson.id}%>`);
+}
