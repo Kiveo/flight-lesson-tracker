2c9bb89e8d57360fdee4858ed5fff9d3dac1cc77
Maverick Tesini <mavericktesini@gmail.com>
2018-08-28 16:10:00 -0400

    skeleton js file for instructor lessons ajax, add supression to turbolinks

---
+++ app/assets/javascripts/instructor_lessons.js
@@ -0,0 +1,53 @@
+// setup listener, after page load
+$(function() {
+  $.get('/lessons', function() {
+    console.log("get lessons");
+  })
+})
+
+// GET the next lesson from the controller
+function nextLesson(url) {
+  console.log(`clicked the next link`);
+  $.get(url, function() {
+    console.log("data requested");
+  }).done(function(data) {
+    var jslesson = new Lesson(data)
+    var lessonDisplay = jslesson.renderLesson()
+  });
+}
+
+// create an object from the data
+function Lesson(attributes) {
+  console.log("You called Lesson Prototype");
+  console.log(attributes);
+  this.student = attributes.student.name;
+  this.instructor = attributes.instructor.name;
+  this.description = attributes.description;
+  this.lessonDatetime = attributes.lesson_datetime;
+  this.next = attributes.next
+  this.previous = attributes.previous
+}
+
+//renderLesson method for the prototype
+Lesson.prototype.renderLesson = function() {
+  let $prev = $('#previousLesson');
+  let $next = $('#nextLesson');
+
+  $('#lessonStudentName').text(this.student);
+  $('#lessonInstructorName').text(this.instructor);
+  $('#lessonDescription').text(this.description);
+  $('#lessonDatetime').text(this.lessonDatetime);
+
+  if (this.next) {
+    $next.attr('href', `${this.next.id}.json`);
+    $next.text("Next");
+  } else {
+    $next.text("End of List");
+  }
+  if (this.previous) {
+    $prev.attr('href', `${this.previous.id}.json`);
+    $prev.text("Previous");
+  } else {
+    $prev.text("End of List");
+  }
+}
