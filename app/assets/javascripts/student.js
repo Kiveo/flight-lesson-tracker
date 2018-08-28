1c5ade0cd881b7bb5b3976ec33ce4be82b3a1a05
Maverick Tesini <mavericktesini@gmail.com>
2018-08-28 19:06:54 -0400

    stub functionality for ajax to create action on new student

--- app/assets/javascripts/student.js
+++ app/assets/javascripts/student.js
@@ -2,6 +2,18 @@
   $(function() {
     $("#new_student").on('submit', function(e){
       e.preventDefault();
-      console.log("INTERCEPT!");
+      // 1. capture form url
+      let student_url = this.action;
+      // 2. translate form data
+      var studentData = {
+        'authenticity_token': $("input[name='authenticity_token']").val(),
+        'student': {
+          'name': $('#student_name').val(),
+          'student_id': $('#student_student_id').val()
+        }
+      }
+      // use url and data to submit a post request
+      $.post( "/students", $('#new_student').serialize() )
+        
     });
   });
